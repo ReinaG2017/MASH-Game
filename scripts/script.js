e87d1dbb0cbc28cd660f6a25e768fa29d1dc9652
ReinaG2017 <89498550+ReinaG2017@users.noreply.github.com>
2021-09-04 12:58:23 -0500

    updated script, missing portion and not displaying answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-/ New function called random_number that takes one parameter, num (or a number)
+// New function called random_number that takes one parameter, num (or a number)
 function random_number(num) {
 	// Get a random number between 0 and a passed-in number
 	var num = num || 4  // If no number passed in, default to 4
